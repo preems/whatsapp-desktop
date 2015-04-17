e8ba82cee78635b844e2ad1f92fe1b87718a365d
Preetham MS <me@preetham.in>
2015-04-16 21:05:55 -0400

    Save window state, hide window instead of close.
    Signed-off-by: Preetham MS <me@preetham.in>

--- src/app.js
+++ src/app.js
@@ -2,23 +2,45 @@ var gui = require('nw.gui');
 var win = gui.Window.get();
 
 
-$('document').ready(function() {
+var platform = process.platform;
+platform = /^win/.test(platform) ? 'win'
+         : /^darwin/.test(platform) ? 'osx64'
+         : 'linux' + (process.arch == 'ia32' ? '32' : '64');
 
-	
-//	win.setBadgeLabel("10");
+if(platform==='win') {
+	var tray = new gui.Tray({ title: 'WhatsApp', icon: 'WhatsApp.png' });
+	var menu = new gui.Menu();
+    menu.append(new gui.MenuItem({ label: 'Open WhatsApp', click: function() { win.show(); } }));
+    menu.append(new gui.MenuItem({ label: 'Close WhatsApp', click: function() { win.close(true); } }));
+    tray.menu = menu;
+}         
+
+win.on('close',function(force) {
+	if (force) {
+		win.close(true);
+	}
+	else {
+		win.hide();
+	}
+});
+
+ gui.App.on('reopen', function() {
+    win.show();
+  });
+
+
+$('document').ready(function() {
 
 	var msgCountReg = /\((\d)\)/;
 
 	setInterval(function() {
 		var res = msgCountReg.exec($("#app").contents().find("title").text());
-		alert($("#app").contents().find("title").text());
 		if (res && res.length>0) {
 			var badge=res[1];
 		}
 		else {
 			var badge ='';
 		}
-		// alert(badge);
 		win.setBadgeLabel(badge);
 	},200);
 
@@ -29,6 +51,4 @@ $('document').ready(function() {
 	});
 
 });
-
-
 
